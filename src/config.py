ab429357f9a75ac4b76ea84183f4f5a9f7005e11
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-11-16 00:04:58 +0400

    Configuration file and start program

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+STEP = 0
+URL = "http://192.168.3.181/port"
+
---
+++ start.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+os.chdir(parent_dir)
+
+scripts = [
+    'sim-roulette-python.src.main'
+]
+
+for script in scripts:
+    subprocess.run(["python", "-m", script])
