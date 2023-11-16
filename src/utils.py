61efeeb14775e7bcdbc1f6c6d92f57710fafbce1
Даниил Костенков <shlyapp.istu@gmail.com>
2023-11-16 12:39:57 +0400

    Adding function to get phone in sms

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,10 @@
 def parse_response(response_text: str) -> tuple:
+    """Возвращает шаг операции и реультат"""
     couples = response_text.replace("#", "").split("!")
     return (couples[0], couples[1])
+
+
+def get_number_from_sms(sms_text: str) -> str:
+    """Возвращает номер телефона из смс оператора"""
+    couples = sms_text.split()
+    return couples[-2]
